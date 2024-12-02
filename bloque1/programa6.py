69d3d5b2e9ffd44086b21e9d6b9d3343ceb9d9fa
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 15:39:10 -0800

    Create programa6.py

---
+++ bloque1/programa6.py
@@ -0,0 +1,12 @@
+#Programa 6. Solicitar la base y la altura de un triangulo
+#Fecha de elaboraciÓn: 16/10/24
+#Elaborado por: JOCELYN DE LUNA
+
+base = float (input("Introduce la base del triangulo:"))
+altura = float (input ("Introduce la altura del triangulo:"))
+
+#Calcular el area
+area = (base * altura)/2
+
+#Mostrar el resultado
+print ("El area del triangulo es: " , area)
