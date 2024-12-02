e9147241a3d1930407c6485484dfbba7c7042672
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 15:51:20 -0800

    Create programa1.12.py

---
+++ bloque1/programa1.12.py
@@ -0,0 +1,16 @@
+#Programa.1.12 COMPUERTAS OR Y AND
+#Fecha de elaboraciÓn: 18/10/24
+#Elaborado por: JOCELYN DE LUNA
+
+print("Compuertas or")
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+print("\n")
+
+print("Compuertas and")
+print(False and False)
+print(True and False)
+print(True and True)
+print("\n")
