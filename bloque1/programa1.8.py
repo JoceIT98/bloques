1844f6b9fc0addc4b2e181147ab20ae5d84bc9f8
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 15:43:42 -0800

    Create programa1.8.py

---
+++ bloque1/programa1.8.py
@@ -0,0 +1,10 @@
+# programa 1.8 Calcular el numero de minutos, horas, meses dando el numero de dias
+# fecha: 16/10/2024
+# nombre: JOCELYN DE LUNA
+dias = int(input("ingrese los dias: "))
+horas = dias * 24
+minutos = horas * 60
+meses = dias / 30
+print("las horas de los dias son: " + str(horas))
+print("los minutos de los dias son: " + str(minutos))
+print("los meses de los dias son: " + str(meses))
