5d68e0d071f5d8f74a365db81b81a7be8f426732
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 16:28:02 -0800

    Update programa1.12.py

--- bloque1/programa1.12.py
+++ bloque1/programa1.12.py
@@ -1,16 +1,16 @@
-Operador or (Compuerta lógica OR):
+#Programa.12 COMPUERTAS OR Y AND
+#Fecha de elaboraciÓn: 18/10/24
+#Elaborado por: JOCELYN DE LUNA
 
-El operador or devuelve True si al menos uno de los operandos es True. Si ambos operandos son False, el resultado es False.
-False or False: El resultado es False porque ambos operandos son False.
-False or True: El resultado es True porque al menos uno de los operandos es True.
-True or False: El resultado es True porque al menos uno de los operandos es True.
-True or True: El resultado es True porque ambos operandos son True.
-Operador and (Compuerta lógica AND):
+print("Compuertas or")
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+print("\n")
 
-El operador and devuelve True solo si ambos operandos son True. Si cualquiera de los operandos es False, el resultado es False.
-False and False: El resultado es False porque ambos operandos son False.
-True and False: El resultado es False porque uno de los operandos es False.
-True and True: El resultado es True porque ambos operandos son True.
-Impresión de resultados:
-
-El código imprime los resultados de las expresiones lógicas con los operadores or y and, con saltos de línea entre cada conjunto de resultados.
+print("Compuertas and")
+print(False and False)
+print(True and False)
+print(True and True)
+print("\n")
