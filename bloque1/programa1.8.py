713acec66ee235c5b17a6ba88fb6bb2b37dd7ef0
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 16:14:50 -0800

    Update programa1.8.py

--- bloque1/programa1.8.py
+++ bloque1/programa1.8.py
@@ -1,10 +1,13 @@
-# programa 1.8 Calcular el numero de minutos, horas, meses dando el numero de dias
-# fecha: 16/10/2024
-# nombre: JOCELYN DE LUNA
-dias = int(input("ingrese los dias: "))
-horas = dias * 24
-minutos = horas * 60
-meses = dias / 30
-print("las horas de los dias son: " + str(horas))
-print("los minutos de los dias son: " + str(minutos))
-print("los meses de los dias son: " + str(meses))
+Entrada de datos:
+
+dias = float(input("Ingresa el número de días:")): Solicita al usuario ingresar un número de días, que se convierte a un valor de tipo float para permitir entradas decimales.
+Cálculos:
+
+Horas: horas = dias * 24 calcula el número total de horas, multiplicando los días ingresados por 24 (número de horas en un día).
+Minutos: minutos = horas * 60 calcula el número total de minutos, multiplicando las horas obtenidas por 60 (número de minutos en una hora).
+Meses: meses = dias / 30 calcula el número aproximado de meses, dividiendo los días ingresados por 30 (promedio de días en un mes).
+Impresión de resultados:
+
+print("El resultado de horas es:", str(horas)): Muestra el número de horas calculadas.
+print("El resultado de minutos es:", str(minutos)): Muestra el número de minutos calculados.
+print("El resultado de meses es:", str(meses)): Muestra el número de meses calculados.
