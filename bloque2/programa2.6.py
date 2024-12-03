a602d883006fd80814e3688b14c157168e62d833
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 17:23:54 -0800

    Create programa2.6.py

---
+++ bloque2/programa2.6.py
@@ -0,0 +1,9 @@
+#Programa.6 Word = "hamster"
+#Fecha de elaboración: 22/10/24
+#Elaborado por: JOCELYN DE LUNA
+
+word="hamster"
+print(word[-1]) #imprime r por comienxo de derecho a izquierdo en negativo
+print(word[1:-1]) #imprime amster
+print(word[-3:])#imprime ter
+print(word[:3]) #imprime ham
