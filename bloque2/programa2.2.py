3ab6c8ff54675f2988cff9171af6fa7c957b0aec
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 17:11:16 -0800

    Create programa2.2.py

---
+++ bloque2/programa2.2.py
@@ -0,0 +1,11 @@
+#Programa.2.2 Programa que si  lupita o sol van a la fiesta, entonces no hay fiesta
+#Fecha de elaboración: 22/10/24
+#Elaborado por: JOCELYN DE LUNA
+
+#|A|B||NOT(A OR B)|
+#|0|1||1
+
+print(not(False or False))
+print(not(False or True))
+print(not(True or False))
+print(not(True or True))
