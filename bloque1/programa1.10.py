507aaeb874485b86f60422464046f0277fe33398
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 16:15:54 -0800

    Update programa1.10.py

--- bloque1/programa1.10.py
+++ bloque1/programa1.10.py
@@ -1,7 +1,14 @@
-# Programa 1.10 caracteres especiales
-# fecha: 17/10/2024
-# nombre: JOCELYN DE LUNA
-print("hola, \nestos son algunos de los caracteres especiales")
-# alt 165 para la ñ
-print("estoy feliz \u263A\t\u2615\t\u2650")
-print("alineando \t\u263A")
+print():
+
+La función print() se usa para imprimir un mensaje en la consola.
+Secuencia de escape \t:
+
+El \t es un tabulador. Inserta un espacio en blanco grande (tabulación) entre los elementos impresos, para alinear las palabras o caracteres.
+Caracteres Unicode:
+
+\u263A: Representa el símbolo de la cara sonriente ☺ en Unicode.
+\u265D: Representa el símbolo de la torre de ajedrez (reina) ♝ en Unicode.
+Impresión:
+
+En la primera línea, imprime "Estoy feliz", seguido de un tabulador, luego el símbolo ☺, más un tabulador y el símbolo ♝.
+En la segunda línea, imprime "Alineado", seguido del mismo formato.
