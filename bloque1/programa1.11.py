80d1a8baa9dfd5fe02b7c3a63022d72ef68ebe16
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-02 16:23:49 -0800

    Update programa1.11.py

--- bloque1/programa1.11.py
+++ bloque1/programa1.11.py
@@ -1,33 +1,46 @@
-Operador de Igualdad (==):
+#Programa.11  Analizar los operadores de comparaciÓn
+#Fecha de elaboraciÓn: 20210/10/18
+#Elaborado por: ANDREA GUADALUPE JIMENEZ CARDENAS
 
-5==7: Compara si 5 es igual a 7, y el resultado es False.
-5==5: Compara si 5 es igual a 5, y el resultado es True.
-5.0==5: Compara si 5.0 es igual a 5. En Python, el valor 5.0 y 5 son considerados iguales, por lo que el resultado es True.
-Operador Diferente de (!=):
+#operadores de comparacion == , !=, >=, <=, >, <
 
-8!=7: Compara si 8 es diferente de 7, y el resultado es True.
-8!=5: Compara si 8 es diferente de 5, y el resultado es True.
-7.0!=5: Compara si 7.0 es diferente de 5, y el resultado es True.
-Operador Mayor o Igual (>=):
+print("Operador de IGUAL (==)")
+print(5==7) #respuesta False
+print(5==5) #respuesta True
+print(5.0==5) #respuesta True
+print("\n") #salto de linea y retorno de carro
 
-8>=7: Compara si 8 es mayor o igual a 7, y el resultado es True.
-8>=5: Compara si 8 es mayor o igual a 5, y el resultado es True.
-7.0>=5: Compara si 7.0 es mayor o igual a 5, y el resultado es True.
-Operador Menor o Igual (<=):
 
-8<=7: Compara si 8 es menor o igual a 7, y el resultado es False.
-8<=5: Compara si 8 es menor o igual a 5, y el resultado es False.
-7.0<=5: Compara si 7.0 es menor o igual a 5, y el resultado es False.
-Operador Mayor (>):
+print ("Operador DIFERENTE O IGUAL (!=)")
+print(8!=7) #respuesta True
+print(8!=5) #respuesta True
+print(7.0!=5) #respuesta True
+print("\n") #salto de linea y retorno de carro
 
-8>7: Compara si 8 es mayor que 7, y el resultado es True.
-8>5: Compara si 8 es mayor que 5, y el resultado es True.
-7.0>5: Compara si 7.0 es mayor que 5, y el resultado es True.
-Operador Menor (<):
+print ("Operador MAYOR O IGUAL (>=)")
+print(8>=7) #respuesta true
+print(8>=5) #respuesta true
+print(7.0>=5) #respuesta true
+print("\n") #salto de linea y retorno de carro
 
-8<7: Compara si 8 es menor que 7, y el resultado es False.
-8<5: Compara si 8 es menor que 5, y el resultado es False.
-7.0<5: Compara si 7.0 es menor que 5, y el resultado es False.
-Símbolo Unicode:
+print ("Operador MENOR O IGUAL (<=)")
+print(8<=7) #respuesta false
+print(8<=5) #respuesta false
+print(7.0<=5) #respuesta false
+print("\n") #salto de linea y retorno de carro
+
+print ("Operador MAYOR (>)")
+print(8>7) #respuesta True
+print(8>5) #respuesta True
+print(7.0>5) #respuesta True
+print("\n") #salto de linea y retorno de carro
+
+print ("Operador MENOR (<)")
+print(8<7) #respuesta false
+print(8<5) #respuesta false
+print(7.0<5) #respuesta false
+print("\n") #salto de linea y retorno de carro
+
+print("\u263A" * 20)
 
 print("\u263A" * 20): El código \u263A representa el símbolo de la carita sonriente (☺) en Unicode. Este símbolo se imprime 20 veces, formando una línea de caritas sonrientes.
