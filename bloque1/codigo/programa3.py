c63475b39401cd278bfa659fc235a65cecb7480e
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-03 16:50:47 -0800

    Create programa3.py

---
+++ bloque1/codigo/programa3.py
@@ -0,0 +1,17 @@
+#Programa3. Programa que imprima 3 datos diferentes, dias de la semana,Triangulo,conversiones
+#Fecha de elaboraciÓn: 14/10/24
+#Elaborado por: JOCELYN DE LUNA
+
+
+#Imprimir el numero de dias en una semana
+print ("Cuantos dia tiene la semana?")
+print (7)
+
+#Imprimir el nombre de una figura de 3 lados 
+print ("Nombre de una figura de 3 lado")
+print ("triangulo")
+
+#Imprimir a cuantos metros equivale 250 cm
+#tipo de dato flotante(float)
+print ("A cuantos metros equivale 250 cm?")
+print (2.5)
