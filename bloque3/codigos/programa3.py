5bdb6849eb84a99a4c09d969ea5d249659597113
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 12:09:22 -0800

    Create programa3.py

---
+++ bloque3/codigos/programa3.py
@@ -0,0 +1,12 @@
+#Programa 3 Identificación de tipos de datos
+# Identificación de tipos de datos str, int y float
+# Fecha: 2024/10/28
+# Elaborado por: Evelyn Jocelyn León de Luna
+variable = input("Ingresa tu edad: ")
+print(type(variable))
+ 
+variable = int(variable) #Convertir la variable a tipo entero
+print(type(variable))
+ 
+variable = float(variable) #Convertir la variable a tipo flotante
+print(type(variable))
