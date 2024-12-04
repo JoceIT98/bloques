1efb8d0ec085454191724ed3efb16bf9013ef709
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 12:12:19 -0800

    Create programa6.py

---
+++ bloque3/codigos/programa6.py
@@ -0,0 +1,14 @@
+# Programa 6 Listas 
+# Fecha: 2024/10/30
+# Elaborado por: Evelyn Jocelyn León de Luna
+print("Lista de enteros")
+print(["5, 8, 10"])
+
+print("\nLista de textos")
+print(["Manzana", "Piña", "Naranja"])
+
+print("\nLista de booleanos")
+print(["True, False, True"])
+
+print(("\nLista vacía"))
+print([])
