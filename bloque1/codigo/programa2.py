215517c6ea801954fc95114db4a85ec093ba4a42
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-03 16:48:58 -0800

    Create programa2.py

---
+++ bloque1/codigo/programa2.py
@@ -0,0 +1,6 @@
+#Programa2. Programa que solicita el nombre y lo imprime con un mensaje personalizado
+#Fecha de elaboraciÓn: 14/10/24
+#Elaborado por: JOCELYN DE LUNA
+
+nombre = input ("Ingresa tu nombre:")
+print("Hola" + nombre + ",Que bonito es tu nombre")
