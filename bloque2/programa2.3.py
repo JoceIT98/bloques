ef8e35e5521cb29d699bfc06981a166bf912b11c
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 13:00:13 -0800

    Delete bloque2/programa2.3.py

--- bloque2/programa2.3.py
+++
@@ -1,6 +0,0 @@
-#Programa.2.3 Comprueba el funcionamiento de la adiccion y concatenacón de varias variables
-#Fecha de elaboración: 22/10/24
-#Elaborado por: JOCELYN DE LUNA
-
-
-print("Mi" + "nombre"+ "es" + "Edinguer")
