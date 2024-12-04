71ed9bbe4423773003c78a4f701e0ccdb1341479
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 12:14:52 -0800

    Create programa9.py

---
+++ bloque3/codigos/programa9.py
@@ -0,0 +1,13 @@
+# Programa 9 Compare puntos con todas las demás listas 
+# Fecha: 2024/10/31
+# Elaborado por: Evelyn Jocelyn León de Luna
+puntos = [10, 30, 20]
+puntos_2 = [10, 30, 20]
+ordenados = [10, 20, 30]
+puntos_texto = ["10", "20", "30"]
+print(puntos==puntos_2) #True
+print(puntos==ordenados) #False
+print(puntos==puntos_texto) #False
+print(puntos!=puntos_2) #False
+print(puntos!=ordenados) #True 
+print(puntos!=puntos_texto) #True
