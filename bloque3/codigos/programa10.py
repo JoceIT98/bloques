f6544e5d361c87bf18fe8d3d5e8d1ce2b2f84864
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 12:15:34 -0800

    Create programa10.py

---
+++ bloque3/codigos/programa10.py
@@ -0,0 +1,8 @@
+# Programa 10 List Membership
+# Fecha: 2024/10/31
+# Elaborado por: Evelyn Jocelyn León de Luna
+nombres = ["Choto", "Emilio", "Luis"]
+print("Luis" in nombres) #True
+print("Emi" in nombres) #False
+print("Javier" in nombres) #False
+print("Jose" not in nombres) #True
