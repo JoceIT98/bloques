815e5f9a4262b3bbc4c8350f49aa8fc8aae5e20d
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-03 16:54:59 -0800

    Create programa6.py

---
+++ bloque1/codigo/programa6.py
@@ -0,0 +1,8 @@
+# Programa 1.7 que calcule el area de un circulo y el perimetro
+# fecha: 15/10/2024
+# nombre: JOCELYN DE LUNA
+radio = float(input("ingresa el valor del radio: "))
+area = 3.1416 * radio ** 2
+perimetro = 2 * 3.1416 * radio
+print("el area del circulo es: " + str(area))
+print("el perimetro del circulo es: " + str(perimetro))
