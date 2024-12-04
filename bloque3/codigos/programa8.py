c19ae785750221b3d121961c7d9bc3ce4f49790e
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 12:14:00 -0800

    Create programa8.py

---
+++ bloque3/codigos/programa8.py
@@ -0,0 +1,14 @@
+# Programa 8 Operaciones con listas
+# Fecha: 2024/10/30
+# Elaborado por: Evelyn Jocelyn León de Luna
+print("Indexing")
+numeros = [10, 20, 30]
+print(numeros[2])
+print(numeros[0])
+print(numeros[1])
+print(numeros[-1])
+print("\nSubscripting")
+nombres = ["Chorchis", "Choto", "Emiliano", "Pepe el toro"]
+print(nombres[1:])
+print(nombres[1:5])
+print(nombres[-2:])
