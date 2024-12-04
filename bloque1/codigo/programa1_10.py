011b4efb46a4a6395929f9515cb2f0a76f2fec54
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-03 17:10:16 -0800

    Create programa1_10.py

---
+++ bloque1/codigo/programa1_10.py
@@ -0,0 +1,7 @@
+# Programa 1.10 caracteres especiales
+# fecha: 17/10/2024
+# nombre:JOCELYN DE LUNA
+print("hola, \nestos son algunos de los caracteres especiales")
+# alt 165 para la ñ
+print("estoy feliz \u263A\t\u2615\t\u2650")
+print("alineando \t\u263A")
