7aa1dadce486d12e6f2aef8296177ff7bb8d1fb1
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 11:56:20 -0800

    Update and rename programa1 to programa1.py

---
+++ bloque3/codigos/programa1.py
@@ -0,0 +1,12 @@
+# Programa 1 mascotas 
+# Fecha: 2024/10/28
+# Elaborado por: EVELYN JOCELYN LEÓN DE LUNA
+mascota = input("Ingresa el tipo de mascota: ")
+if "perro" in mascota:
+   print("Es un perro")
+elif "gato" in mascota:
+   print("Tienes un gato")
+else:
+   print("Tipo de mascota desconocida")
+   print("Gracias por usar nuestro programa")
+
