b3cd6bad22c3fe2cdc515c1a0610acc094683bf0
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 12:13:05 -0800

    Create programa7.py

---
+++ bloque3/codigos/programa7.py
@@ -0,0 +1,8 @@
+# Programa 7 Tipos de datos Lista y tipos de datos de los elementos 
+# Fecha: 2024/10/30
+# Elaborado por: Evelyn Jocelyn León de Luna
+cadenas = ["Juan", "Pedro", "Chuy"]
+print(cadenas)
+print(cadenas[1])
+print(type(cadenas))
+print(type(cadenas[1]))
