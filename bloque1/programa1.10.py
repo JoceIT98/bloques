671cf5acaea9870ae1501e81ded26200412729ab
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-03 17:09:27 -0800

    Update programa1.10.py

--- bloque1/programa1.10.py
+++ bloque1/programa1.10.py
@@ -1,6 +1,6 @@
-# programa 1.10 caracteres especiales
+# Programa 1.10 caracteres especiales
 # fecha: 17/10/2024
-# nombre: Alexandra Rodriguez Gomez
+# nombre:JOCELYN DE LUNA
 print("hola, \nestos son algunos de los caracteres especiales")
 # alt 165 para la ñ
 print("estoy feliz \u263A\t\u2615\t\u2650")
