1b56e9c1c57bb253788f61f3ebd7204fcbed0301
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 11:58:39 -0800

    Create programa2.py

---
+++ bloque3/codigos/programa2.py
@@ -0,0 +1,11 @@
+# Programa 2 Programa que Indique si, de dos enteros ingresados, el primero es mayor, igual o menor que el segundo
+# Fecha: 2024/10/28
+# Elaborado por: Evelyn Jocelyn León de Luna
+n1 = int(input("ingresa el numero 1: "))
+n2 = int(input("ingresa el numero 2: "))
+if n1 > n2:
+   print ("El numero " + str(n1) + " es mayor al " + str (n2))
+elif n1 == n2:
+   print("Son números iguales")
+else:
+   print("El numero " + str (n1) + " es menor al número " + str(n2)
