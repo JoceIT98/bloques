814c2f32ab17c9509818c6df540a4c0a8ca9fa95
JoceIT98 <evelyndeluna083@gmail.com>
2024-12-04 20:45:29 -0800

    Create programa12.py

---
+++ bloque3/codigos/programa12.py
@@ -0,0 +1,15 @@
+# Programa 12 Método pop
+# Fecha: 2024/10/31
+# Elaborado por: Evelyn Jocelyn León de Luna
+# Creamos una lista 
+mi_lista = ["Rojo", "Verde", "Morado", "Azul", "Naranja"]
+
+# Usamos pop para eliminar y devolver el último elemento
+ultimo_elemento = mi_lista.pop()
+print("Elemento eliminado:", ultimo_elemento)
+print("Lista después de usar pop:", mi_lista)
+
+# Usamos pop para eliminar y devolver el elemento en la posición 1
+elemento_en_posicion_1 = mi_lista.pop(1)
+print("Elemento eliminado en la posición 1:", elemento_en_posicion_1)
+print("Lista después de usar pop en la posición 1:", mi_lista)
